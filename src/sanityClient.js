b46c2e933ee61054e79ee61150134433089ae3d5
Codeineator <86856064+Codeineator@users.noreply.github.com>
2026-04-14 20:10:48 +0530

    Update sanityClient.js

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: 'efr4il2n',
-  dataset: 'production',
-  useCdn: true, 
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  useCdn: true, // true for fast response, false for latest data
   apiVersion: '2024-03-14', 
 })
